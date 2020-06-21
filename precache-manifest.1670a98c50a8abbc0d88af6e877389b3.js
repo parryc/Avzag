892823bea83f3de7f907520a57252a3ec80f0853
alkaitagi <alkaitagi@outlook.com>
2020-06-21 22:19:47 +0000

    Deploying to gh-pages from  @ a41da8752846b2b3eb64f4b5343684b39785eb08 🚀

--- precache-manifest.1670a98c50a8abbc0d88af6e877389b3.js
+++ precache-manifest.1670a98c50a8abbc0d88af6e877389b3.js
@@ -848,7 +848,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Aevzag/languages/Dargin/Kaitag/audio/я.m4a"
   },
   {
-    "revision": "187573377a1dc025d8519281ff5255e0",
+    "revision": "f53346876abc9d1e9a85569165aa10a1",
     "url": "/Aevzag/languages/Dargin/converters.json"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/Aevzag/precache-manifest.65ac8642100fd4fe9befa28f049c3231.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/Aevzag/precache-manifest.1670a98c50a8abbc0d88af6e877389b3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // This is the service worker with the Cache-first network
 
