3a8c5bbd1ce716d0333569c64ced5a5402ac0f24
alkaitagi <alkaitagi@outlook.com>
2020-10-12 10:56:38 +0000

    Deploying to gh-pages from  @ 926884d63cab3b2600be5be53aa9abc11ef10255 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.89cc6126e90f389d7a49830a88a75e42.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.dad094163f82151dce04bb709c052128.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // This is the service worker with the Cache-first network
 
